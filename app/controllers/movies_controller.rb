a859cb34066b47eafcb07fc6a372f69941791a26
P.Szulczynski <pszulczynski@gmail.com>
2012-08-09 10:54:46 +0200

    Name and date sorting working

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,15 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    sort_titles = params[:sort_titles]
+    sort_date = params[:sort_date]
+    if !sort_titles.nil? and sort_titles == "true"
+      @movies = Movie.order("title")
+    elsif !sort_date.nil? and sort_date == "true"
+      @movies = Movie.order("release_date")
+    else
+      @movies = Movie.all
+    end
   end
 
   def new
@@ -15,6 +23,7 @@ class MoviesController < ApplicationController
   end
 
   def create
+    debugger
     @movie = Movie.create!(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
